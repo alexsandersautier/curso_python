f13e09b434a8bc71c990d331ffad0a8d95f75bf8
Alexsander Sautier <alexsanderesramos@gmail.com>
2022-09-19 07:55:33 -0300

    ultima etapa

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 largura = float = 5
 altura = float = 2.9
 profundidade = 3
 print(2 * (largura+profundidade)*altura)
 print(largura*profundidade)
+=======
+largura: float = float(input('Qual o valor da largura do cômodo:'))
+profundidade: float = float(input('Qual o valor da profundidade do comodo:'))
+altura: float = 2.9
+
+print(largura)
+print(profundidade)
+
+print("A area das paredes é: " ,
+      (2*(largura+profundidade)*altura))
+>>>>>>> ef9edc43360159da041ba2871d1a1e51eeb2c8ef
