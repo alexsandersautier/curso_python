ac385a30007ae5df563b68797289f0159afc09a6
Alexsander Sautier <alexsanderesramos@gmail.com>
2022-09-19 11:31:12 -0300

    implementação dos calculos na classe calculadora e chamada no main

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ largura: float = float(input('Qual o valor da largura do cômodo:'))
 profundidade: float = float(input('Qual o valor da profundidade do comodo:'))
 altura: float = 2.9
 
-calc.area_paredes: float = (2*(largura+profundidade)*altura)
-print("A area das paredes é: " , calc.area_paredes)
+print("A area das paredes é: " ,
+      calc.calcular_area_paredes(largura,profundidade,altura)
+)
 
-calc.area_teto: float = largura * profundidade
-print('A area do teto é:', calc.area_teto)
+print('A area do teto é:',
+      calc.calcular_area_teto(largura,profundidade)
+)
 
 print('A litragem de de tinta necessária é: ',
-      (calc.area_paredes + calc.area_teto)/10
+      calc.calcular_litragem_necessario()
 )
