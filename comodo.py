42744d720620b4cd1b16c003d810e33a8562e9c9
Alexsander Sautier <alexsanderesramos@gmail.com>
2022-09-19 13:52:10 -0300

    criação da classe comodo, restrição de atributos

---
+++ comodo.py
@@ -0,0 +1,8 @@
+class Comodo:
+    largura:float
+    profundidade:float
+
+    def __init__(self, largura, profundidade):
+        self.largura = float(largura)
+        self.profundidade = float(profundidade)
+        self.altura = 2.9
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from calculadora import Calculadora
+from comodo import Comodo
 
 calc = Calculadora()
 
-largura: float = float(input('Qual o valor da largura do cômodo:'))
-profundidade: float = float(input('Qual o valor da profundidade do comodo:'))
-altura: float = 2.9
+comodo = Comodo(
+      input('Qual o valor da largura do cômodo:'),
+      input('Qual o valor da profundidade do comodo:')
+)
 
 print("A area das paredes é: " ,
-      calc.calcular_area_paredes(largura,profundidade,altura)
+      calc.calcular_area_paredes(
+            comodo
+      )
 )
 
 print('A area do teto é:',
-      calc.calcular_area_teto(largura,profundidade)
+      calc.calcular_area_teto(
+            comodo
+      )
 )
 
 print('A litragem de de tinta necessária é: ',
